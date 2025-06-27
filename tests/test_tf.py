95b3c285427ffbabd36c6ca77a0c08ecce694d62
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2025-06-27 16:31:54 -0700

    Possible bare-minimum

---
+++ tests/test_tf.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+import ddlcloud_tf_base_schemas 
+
+class TestCli(TestCase):
+    def test_schemas(self):
+        ddlcloud_tf_base_schemas.TFSet(configs={}, module_id="module", version="1.0")
